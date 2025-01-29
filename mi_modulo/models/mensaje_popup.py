0b7d3efebfea74794a4fa822c541dd5b1e53476f
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2025-01-29 10:19:55 -0500

    Update mensaje_popup.py

--- mi_modulo/models/mensaje_popup.py
+++ mi_modulo/models/mensaje_popup.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class MensajePopup(models.TransientModel):
   _name = 'mensaje.popup'
 
-  mensaje = fields.text(string="Mensaje")
+  mensaje = fields.Text(string="Mensaje")
