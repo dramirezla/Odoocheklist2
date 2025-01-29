74f6e1756d219778bfd47a7d26fb65389cc7bbce
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2025-01-29 08:53:43 -0500

    Update mi_modelo.py

--- mi_modulo/models/mi_modelo.py
+++ mi_modulo/models/mi_modelo.py
@@ -2,12 +2,6 @@ from odoo import models, fields, api
 
 class MiModelo(models.Model):
     _inherit = "sale.order"
-    _name = 'mi.modelo'
-    _description = 'Descripción de mi modelo'
-
-    name = fields.Char(string='Nombre', required=True)
-    descripcion = fields.Text(string='Descripción')
-    fecha = fields.Date(string='Fecha')
 
     def mostrar_partes_seleccionadas(self):
         return {
