b3df0f91f0a0f5719cf4459225f1812194922bae
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2025-01-29 09:29:52 -0500

    Update mi_modelo.py

--- mi_modulo/models/mi_modelo.py
+++ mi_modulo/models/mi_modelo.py
@@ -3,11 +3,13 @@ from odoo import models, fields, api
 class MiModelo(models.Model):
     _inherit = "sale.order"
 
+    campo_a_mostrar = fields.Text(string='Campo a mostrar')
+
     def mostrar_partes_seleccionadas(self):
         return {
             'type': 'ir.actions.client',
             'tag': 'display_notification',
             'params': {
-                'title': 'Hola mundo'
+                'title': campo_a_mostrar
             }
         }
